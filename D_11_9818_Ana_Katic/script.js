ba678425967fdc900b766101d0d34488c4d25fed
anakatic <katiic.ana@gmail.com>
2021-05-09 17:33:55 +0200

    11 domaci

--- D_11_9818_Ana_Katic/script.js
+++ D_11_9818_Ana_Katic/script.js
@@ -2,8 +2,8 @@
 /*1.Zadatak
 Napisati funkciju suma kojoj se prosleđuju parametri n i m, dok funkcija vraća sumu kubova brojeva od n do m.
 Pozvati funkciju i njen rezultat ispisati u konzoli.*/
-let suma1 = 0;
 function suma(n, m){
+    let suma1 = 0;
     for (i = n; i <= m; i++){
         suma1 += (i **3);
     }
@@ -17,9 +17,10 @@ Napisati funkciju godine kojoj se prosleđuje parametar godRodjenja.
 Parametar godRodjenja predstavlja godinu rođenja neka osobe. Funkcija treba da vrati koliko godina ima ta (prosleđena) osoba. 
 Broj godina osobe izračunati na osnovu prosleđene godine rođenja i trenutne godine koju preuzimate sa računara. 
 Pozvati funkciju i dobijeni rezultat ispisati u konzoli.*/
-let datum = new Date();
-let god = datum.getFullYear();
+
 function godine(godRodjenja){
+    let datum = new Date();
+    let god = datum.getFullYear();
     let brGodina = god - godRodjenja;
     return brGodina;
 }
@@ -28,8 +29,9 @@ console.log(godine(1997));
 /*3.Zadatak
 Napisati funkciju deljiv kojoj se prosleđuju tri parametra n, m i k. Funkcija na ekranu treba da ispiše broj brojeva od n do m koji su deljivi brojem k. Pozvati funkciju.*/
 
-let brBrojeva = 0;
+
 function deljiv(n, m, k){
+    let brBrojeva = 0;
     for (i = n; i <= m; i++){
         if(i % k == 0){
             brBrojeva++;
